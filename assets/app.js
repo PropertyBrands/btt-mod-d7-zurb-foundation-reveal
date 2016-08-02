470f99bb57e629f8ea2d8446912cc24deacefd0a
ethanhinson <ethan@bluetent.com>
2016-08-01 22:43:36 -0600

    Initial module implementation

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+(function ($, Drupal, window, document, undefined) {
+  Drupal.behaviors.zurbFoundationReveal = {
+    attach: function(context, settings) {
+      if(typeof(Drupal.settings.zurbFoundationReveals !== 'undefined')) {
+        $('body', context).once('zurb-foundation-reveals', function() {
+          for(var rv in Drupal.settings.zurbFoundationReveals) {
+            var rvobj = $(Drupal.settings.zurbFoundationReveals[rv].reveal);
+            Drupal.attachBehaviors(Drupal.settings.zurbFoundationReveals[rv], settings);
+            $('body', context).append(rvobj);
+          }
+          $(document).foundation();
+        });
+      }
+    }
+  };
+})(jQuery, Drupal, this, this.document);
